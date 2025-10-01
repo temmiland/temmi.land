b526570622a555d8fbb70d0e255dcbd6176f2dad
Temmi Pietsch <welcome@temmi.land>
2025-10-01 20:02:41 +0200

    📝 updated doc

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#
+# Copyright (C) 2025 Temmi Pietsch - All Rights Reserved
+#
+# You may not use, distribute or modify this code without the explicitly
+# permission of the author.
+#
 
 set -e
 
